c7679a162dfd74fdabbc7a042180eb488363b399
sjchakrav <sjchakrav@gmail.com>
2015-07-08 11:06:40 -0400

    added jobvite javascript

--- ERP/js/app.js
+++ ERP/js/app.js
@@ -19,3 +19,15 @@ popup2 = function() {
   generator.document.write('</body></html>');
   generator.document.close();
 };
+
+
+var popup3;
+
+popup3 = function() {
+  var generator;
+  generator = window.open('Find Your Connections', 'finding', 'height=600,width=800');
+  generator.document.write('<html><head><img src="http://i.imgur.com/Ezcy4AS.png" width = "800" height = "600"><img src="http://i.imgur.com/8jwhcuO.png" width = "500" height = "500"><title>Find your Connections</title>');
+  generator.document.write($('#finding').html());
+  generator.document.write('</body></html>');
+  generator.document.close();
+};
